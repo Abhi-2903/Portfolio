9aba92229f3f187de8a0fac90f282b0c4e2da2cd
Abhimanyu <abhimanyuchachan9@gmail.com>
2025-10-05 00:57:04 +0530

    starts from here

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: "asset/source",
+    });
+
+    return config;
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  images: {
+    domains: [],
+  },
+};
